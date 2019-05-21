40221612536060c483b17b0aa70ccc0427a09ec0
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-05-21 16:50:39 -0400

    made changes to everything

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 (() => {
-	// this is a self-invoking function
-	
-	/* python print -> console.log is the same thing */
-	console.log('SVG Assignment');
+	console.log('fired!');
+
+
+	//select elements with JavaScript//
+
+	//setting up a variable using the let keyword,
+	//and using a css selector to find the element we want to use.
+	//let stores the reference to element as a variable (in memory)//
+
+	let svgGraphic = document.querySelector("#badgeSVG"),
+        mainHeadline = document.querySelector(".main-headline"),
+        subHeadline = document.querySelector(".sub-headline"),
+        swapTextButton = document.querySelector(".switch-type")
+
+	//funtions are reusable pieces of code
+	//that you can run any time
+
+	function logMyId() {
+		console.log(this.id);
+
+		this.style.opacity = 0.5;
+	}
+    
+    function swapText(){
+    mainHeadline.textContent = "Now you're something else!";
+    mainHeadline.classList.toggle("selected");
+        
+    subHeadline.textContent = "So are you!";
+    subHeadline.classList.toggle("selected");
+    }
+    
+    // events always go down here
+	svgGraphic.addEventListener("click", logMyId);
+    swapTextButton.addEventListener("mouseover", swapText);
+
 })();
